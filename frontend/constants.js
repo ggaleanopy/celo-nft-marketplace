fdafff7658a7eec7699a0d71684b9b7df937c43a
Gustavo Galeano <ggaleano.py@gmail.com>
2022-08-09 22:48:50 -0400

    First Commit

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/ggaleanopy/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0xc059b24373653AC3128d353F817697ADDcb8eE08";
+
